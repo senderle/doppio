04cb519bb42350e1dcc40885247c0ecf2c5d8bfe
acgmarion <acgmarion@gmail.com>
2017-07-31 20:52:48 +0000

    add unique constraint for secret key

--- settings.py
+++ settings.py
@@ -10,7 +10,6 @@ accountschema =  {
     'secret_key': {
         'type': 'string',
         'required': True,
-        'unique': True
         },
     'roles': {
         'type': 'list',
@@ -24,10 +23,6 @@ ephemeralRecord = {
     'public_methods': ['GET'],
     'public_item_methods': ['GET'],
     'item_title' : 'record',
-    #'additional_lookup': {
-    #    'url': 'regex("[\w]+")',
-    #    'field': 'callNumber',
-    #},
     'allowed_roles': ['superuser', 'admin', 'user'],
     'schema': schema
 }
@@ -35,8 +30,6 @@ ephemeralRecord = {
 
 accounts = {
     'item_title' : 'account',
-#    'public_methods': ['GET','POST'],
-#    'public_item_methods': ['GET','PUT'],
     'additional_lookup': {
         'url': 'regex("[\w]+")',
         'field': 'userid',
