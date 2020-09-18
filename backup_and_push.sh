7a28a060c6779d022e56741fc69507e74c51e56e
Scott Enderle <scott.enderle@gmail.com>
2020-09-18 18:28:00 +0000

    use new backup location in script

--- backup_and_push.sh
+++ backup_and_push.sh
@@ -1,10 +1,10 @@
 # Move most recent data to archive.
-mv -f backups/latest/* backups/dated/
+mv -f data/backups/latest/* data/backups/dated/
 
 # Run the backup routine in a disposable container.
-docker-compose -f production.yml run --rm eve flask exportjson backups/latest/`date +'%Y-%m-%d'`
+docker-compose -f production.yml run --rm eve flask exportjson data/backups/latest/`date +'%Y-%m-%d'`
 
 # Commit and push changes to git.
-git add backups/latest backups/dated
+git add data/backups/latest data/backups/dated
 git commit -m "latest backup"
 git push
